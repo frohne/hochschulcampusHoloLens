f85b7ea5bd64fe3c9f796eed785a0a9f20207e69
JanBalke <jan.balke@stud.hshl.de>
2018-11-19 16:01:30 +0100

    InitiaProject

---
+++ Assets/OwnTapResponder_small.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace HoloToolkit.Unity.InputModule.Tests
+{
+    public class OwnTapResponder_small : MonoBehaviour, IInputClickHandler
+    {
+        public GameObject myCarBody;
+        void Start()
+        {
+        }
+        public void OnInputClicked(InputClickedEventData eventData)
+        {
+            myCarBody.transform.localScale -= 0.05f * myCarBody.transform.localScale;
+
+            eventData.Use();
+        }
+    }
+}
